119d2497e0df390bee6fa4240032cef91a9f5ebc
Thiago Urias <thiagourias_@hotmail.com>
2025-08-02 15:24:41 -0300

    Added some new features on fixit page

--- src/app/fixit/fixit.component.ts
+++ src/app/fixit/fixit.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fixit',
   templateUrl: './fixit.component.html',
   styleUrls: ['./fixit.component.css']
 })
-export class FixitComponent {
+export class FixitComponent implements OnInit {
+  showH1: boolean = false;
+  showP1: boolean = false;
+  showH3: boolean = false;
+  showH4: boolean = false;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.showH1 = true;
+    }, 700);
+    setTimeout(() => {
+      this.showP1 = true;
+    }, 1400);
+    setTimeout(() => {
+      this.showH3 = true;
+    }, 2100);
+        setTimeout(() => {
+      this.showH4 = true;
+    }, 2800);
+  }
 
 }
