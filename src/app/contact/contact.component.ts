fed07c6b41c7a4540d73d2eb31860b9ab28fea17
Thiago Urias <thiagourias_@hotmail.com>
2025-08-05 19:02:13 -0300

    Fixing the form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,27 +12,4 @@ export class ContactComponent {
     email: '',
     mensagem: '',
   };
-
-  onSubmit(form: NgForm) {
-    if (form.valid) {
-      const encoded = Object.keys(this.formData)
-        .map(
-          (key) =>
-            encodeURIComponent(key) + '=' + encodeURIComponent(this.formData[key as keyof typeof this.formData])
-        )
-        .join('&');
-
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: encoded,
-      })
-        .then(() => {
-          window.location.href = '/success';
-        })
-        .catch((error) => alert(error));
-    } else {
-      alert('Por favor, preencha todos os campos obrigatórios.');
-    }
-  }
 }
